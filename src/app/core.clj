cefd1e4c73a443d9a9a8465ac4d2b0c7af18f154
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-06-16 10:35:48 -0300

    initial commit: started project with leiningen

---
+++ src/app/core.clj
@@ -0,0 +1,8 @@
+(ns app.core
+  (:require [com.moclojer.adapters :as adapters]
+            [com.moclojer.server :as server]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
